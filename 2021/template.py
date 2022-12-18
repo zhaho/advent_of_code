966f5523179b3b365118c9f130568585fc17d465
Zhaho <zhaho@Zhahos-MacBook-Pro.local>
2022-12-18 22:34:03 +0100

    Update

--- 2021/template.py
+++ 2021/template.py
@@ -1,2 +1,20 @@
-print("for f in open('input.txt','r'):")
+import sys
+import os
+
+# Take folder as input argument
+path = sys.argv[1]
+newpath = r'./'+path
+
+# If folder does not exist, create
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
+# Create script file
+f = open(newpath+"/"+path+".py","w+")
+f.write("for f in open('input.txt','r'):")
+f.close()
+
+# Create input file
+f = open(newpath+"/input.txt","w+")
+f.close()
     
