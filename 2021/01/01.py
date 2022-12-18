0c3bcc64bab43e60a8dfbb1bb00ae66260e53997
Zhaho <zhaho@Zhahos-MacBook-Pro.local>
2022-12-18 22:22:34 +0100

    Day 1 2021

--- 2021/01/01.py
+++ 2021/01/01.py
@@ -1,1 +1,10 @@
+prevNum, incNum, decNum = 0,0,0
 for f in open('input.txt','r'):
+    line = int(f.strip())
+    if(line > prevNum and prevNum != 0):
+        incNum += 1
+    elif (line < prevNum):
+        decNum += 1 
+    prevNum = line
+
+print("Increased: "+str(incNum)+", Decreased: "+str(decNum))
